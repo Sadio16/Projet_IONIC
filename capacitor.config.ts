46faf7dedf689d9e8568103c56f50d0bda9ecaf6
Coumbourou Sidibe <csidibe@amrtp.local>
2024-05-03 16:38:39 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'firstApp2O24',
+  webDir: 'www'
+};
+
+export default config;
